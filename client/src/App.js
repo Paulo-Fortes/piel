3127a6314dd38c3e7a87060ae66e0eec8b348b62
Paulo Fortes <95700441@ifts24.edu.ar>
2025-06-13 20:46:24 -0300

    chakra ui error de instalacion.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div>
-      Front end
-    </div>
-  );
-}
+import { ChakraProvider } from '@chakra-ui/react'
 
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider> TheRestOfYourApplication </ChakraProvider>
+  )
+}
 export default App;
